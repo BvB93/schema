64e17d7a64696a28f07780bbeb6294213e146917
Bas van Beek <b.f.van.beek@vu.nl>
2020-04-04 19:00:04 +0200

    Added stub files

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+import os
 import codecs
 import sys
 
 from setuptools import setup
 
-version_file = "schema.py"
+version_file = os.path.join(os.getcwd(), "schema", "__init__.py")
 with open(version_file) as f:
     for line in f.read().split("\n"):
         if line.startswith("__version__ ="):
@@ -22,7 +23,6 @@ setup(
     license="MIT",
     keywords="schema json validation",
     url="https://github.com/keleshev/schema",
-    py_modules=["schema"],
     long_description=codecs.open("README.rst", "r", "utf-8").read(),
     long_description_content_type="text/x-rst",
     install_requires=open("requirements.txt", "r").read().split("\n"),
@@ -40,4 +40,6 @@ setup(
         "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: MIT License",
     ],
+    packages=['schema'],
+    package_data={'schema': ['py.typed', '__init__.pyi']},
 )
